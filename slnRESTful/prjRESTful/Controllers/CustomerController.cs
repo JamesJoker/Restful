bc8b462dba89eaf6083a0062c40b09919a011dba
JamesJoker <jocularjoker@gmail.com>
2022-05-25 08:24:50 +0800

    Rename ValuesController with CustomerController

--- slnRESTful/prjRESTful/Controllers/CustomerController.cs
+++ slnRESTful/prjRESTful/Controllers/CustomerController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using prjRESTful.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,35 +11,41 @@ namespace prjRESTful.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class CustomerController : ControllerBase
     {
-        // GET: api/<ValuesController>
+        private NorthwindDbContext _context;
+
+        public CustomerController(NorthwindDbContext context)
+        {
+            _context = context;
+        }
+        // GET: api/<CustomerController>
         [HttpGet]
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/<ValuesController>/5
+        // GET api/<CustomerController>/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
             return "value";
         }
 
-        // POST api/<ValuesController>
+        // POST api/<CustomerController>
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
-        // PUT api/<ValuesController>/5
+        // PUT api/<CustomerController>/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
-        // DELETE api/<ValuesController>/5
+        // DELETE api/<CustomerController>/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
