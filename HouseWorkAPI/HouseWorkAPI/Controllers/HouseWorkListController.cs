0718e441496dc9c392e727c9fa4856fbc5773269
JamesJoker <49804752+JamesJoker@users.noreply.github.com>
2024-10-29 08:46:38 +0800

    Add HouseWorkList Controller

---
+++ HouseWorkAPI/HouseWorkAPI/Controllers/HouseWorkListController.cs
@@ -0,0 +1,19 @@
+﻿using HouseWorkAPI.Modules.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HouseWorkAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HouseWorkListController(HouseWorkService service) : ControllerBase
+    {
+        private readonly HouseWorkService _houseWorkService = service;
+
+        [HttpGet]
+        [Route("members")]
+        public IActionResult Members()
+        {
+            return Ok(_houseWorkService.Members);
+        }
+    }
+}
