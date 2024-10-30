1d4a13bf3192ad523218500bfac8a672a42d6c60
JamesJoker <49804752+JamesJoker@users.noreply.github.com>
2024-10-30 16:00:09 +0800

    Add VueWork
    vue app have not type of uuid or guid, need transfer to string type

--- HouseWorkAPI/HouseWorkAPI/Models/Work.cs
+++ HouseWorkAPI/HouseWorkAPI/Models/Work.cs
@@ -15,4 +15,19 @@ namespace HouseWorkAPI.Models
         [Column("ownergroup")]
         public int[] OwnerGroup { get; set; } = new int[0];
     }
+
+    public class VueWork : Work
+    {
+        public string UUId
+        {
+            get
+            {
+                return Id?.ToString("D") ?? string.Empty;
+            }
+            set
+            {
+                Id = Guid.Parse(value);
+            }
+        }
+    }
 }
